c8055ce7b68255357277aa64bc3c173ef77f4249
Gabe Grundtner <gbgrun10@smumn.edu>
2013-02-19 06:13:11 -0600

    fixed migrations again

--- db/migrate/20130219065150_add_column_to_my_table.rb
+++ db/migrate/20130219065150_add_column_to_my_table.rb
@@ -1,8 +1,8 @@
 class AddColumnToMyTable < ActiveRecord::Migration
   def change
   	self.up
-  	  add_column :Order, :customer_name, :string
-  	  add_column :Order, :customer_id, :integer
+  	  add_column :order, :customer_name, :string
+  	  add_column :order, :customer_id, :integer
 
 
   end
